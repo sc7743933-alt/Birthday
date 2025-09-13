fd68fcb69e646e847e0e8c98538574112651e2f1
sc7743933-alt <sc7743933@gmail.com>
2025-09-13 19:07:21 +0530

    Create Confetti.js

---
+++ Confetti.js
@@ -0,0 +1,27 @@
+// Simple Confetti Animation 🎉
+function startConfetti() {
+  const duration = 5 * 1000; // 5 seconds
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function() {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+    confetti(Object.assign({}, defaults, {
+      particleCount,
+      origin: {
+        x: randomInRange(0.1, 0.9),
+        y: Math.random() - 0.2
+      }
+    }));
+  }, 250);
+}
